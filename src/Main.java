d304c94e78769b8a54f7f53e0f63ef9d94b265e5
AxelJimenezHOLA <axel.jimenez85@uabc.edu.mx>
2024-11-04 20:48:07 -0800

    Cambiar el Main para ejecutar la interfaz

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        JuegoAhorcado ahorcado = new JuegoAhorcado();
-        ahorcado.iniciarJuego();
+        new GUI();
     }
 }
